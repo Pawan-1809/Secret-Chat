6aa5141ae6963bcc6710090e2892f2ab0d188b72
pawan1809kr <pawankr16123114@gmail.com>
2025-08-16 09:45:16 +0530

    c1

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default defineConfig(async () => ({
+export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          (await import("@replit/vite-plugin-cartographer")).cartographer(),
-        ]
-      : []),
   ],
   resolve: {
     alias: {
@@ -32,4 +24,4 @@ export default defineConfig(async () => ({
       deny: ["**/.*"],
     },
   },
-}));
+});
